69bcb800e1a02512ea16e1e17ddd74015d3ce035
lucaoskaique <lucas.kaique175@hotmail.com>
2022-07-30 00:37:24 -0300

    event lerp

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
+import { useRef, useState} from 'react'
 
 function Box() {
   const ref = useRef()
-
+  
   useFrame((state) => {
-    ref.current.position.x = Math.sin(state.clock.elapsedTime) * 2
-    ref.current.rotation.y = Math.sin(state.clock.elapsedTime * 2)
+    // ref.current.position.x = Math.sin(state.clock.elapsedTime) * 2
+    // ref.current.rotation.y = Math.sin(state.clock.elapsedTime * 2)
     // ref.current.rotation.y += 0.01
+    ref.current.position.z = THREE.MathUtils.lerp(ref.current.position.z, clicked ? 1 : 0, 0.1)
   })
 
   return (
     <mesh ref={ref}>
       <boxGeometry/>
-      <meshBasicMaterial color="orange"/>
+      <meshBasicMaterial color="red"/>
     </mesh>
   )
 }
 
 function App() {
-
   return (
   <Canvas>
     <Box />
