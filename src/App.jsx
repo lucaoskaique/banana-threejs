4cad648d3a481440ce1926dd308f692263f7e736
lucaoskaique <lucas.kaique175@hotmail.com>
2022-08-07 17:37:14 -0300

    using usegtlf to load component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useRef} from 'react'
+import { Suspense, useRef, useState} from 'react'
 import { Canvas, useFrame, useThree} from '@react-three/fiber'
+import * as THREE from 'three'
+import { Environment, useGLTF } from '@react-three/drei'
 
-function Box() {
-  const ref = useRef()
-  const { viewport } = useThree()
+function Box({z}) {
+  const ref = useRef()  
+  const { viewport, camera } = useThree()
+  const { width, height } = viewport.getCurrentViewport(camera, [0,0,z])
+  
+  const [data] = useState({
+    x: THREE.MathUtils.randFloatSpread(2),
+    y: THREE.MathUtils.randFloatSpread(height)
+  })
   
   useFrame((state) => {
-    ref.current.position.y += 0.01
-    if(ref.current.position.y > viewport.height / 1.5 ) {
-      ref.current.position.y = -viewport.height / 1.5 
+    ref.current.position.set(data.x * width, (data.y += 0.5), z)
+    if(data.y > height / 1.5 ) {
+      data.y = -height / 1.5 
     }
   })
 
@@ -20,12 +28,28 @@ function Box() {
   )
 }
 
-function App() {
+function Horseshoe(props){
+  // const { scene } = useGLTF('https://thinkuldeep.com/modelviewer/Astronaut.glb')
+  const { scene } = useGLTF('/ferradura-v1-v2.glb')
+
+  scene.traverse( obj => {
+    if(obj.isMesh) {
+      obj.material.color.set("pink")      
+    }
+  })
+  return (<primitive object={scene} {...props} />)
+}
+
+function App({count = 100}) {
   return (
   <Canvas>
-    <Box />
-    <Box />
-    <Box />
+    <ambientLight intensity={1.2} />
+    <spotLight position={[10, 10, 10]} intensity={2} />
+    <Suspense fallback={null}>
+      <Horseshoe scale={0.05}/>
+      <Environment preset='sunset'/>
+    </Suspense>
+    {/* {Array.from({length: count}, (_, i) => (<Box key={i} z={-i} />))} */}
   </Canvas>
   )
 }
